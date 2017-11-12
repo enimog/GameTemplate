c4e409b3566873d446b12135131e59a641e09a86
Éric Bisson <dofus-mog@live.fr>
2017-11-12 02:49:38 -0500

    more polished string toolbox

--- GameTemplate/StringToolbox.cpp
+++ GameTemplate/StringToolbox.cpp
@@ -3,14 +3,81 @@
 #include <locale>
 #include <codecvt>
 
-std::wstring StringToolbox::StringToWide(std::string const & string)
+namespace Toolbox
 {
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.from_bytes(string);
-}
+    std::wstring StringToolbox::StringToWide( std::string const & string )
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        return converter.from_bytes( string );
+    }
 
-std::string StringToolbox::WideToString(std::wstring const & string)
-{
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.to_bytes(string);
+    std::string StringToolbox::WideToString( std::wstring const & string )
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        return converter.to_bytes( string );
+    }
+
+    std::vector<std::wstring> StringToolbox::Split( std::wstring const & string, std::wstring const & delimiter )
+    {
+        std::vector<std::wstring> result;
+        auto temp( string );
+        size_t pos = 0;
+
+        while ((pos = temp.find( delimiter )) != std::wstring::npos)
+        {
+            result.push_back(temp.substr( 0, pos ));
+            temp.erase( 0, pos + delimiter.length() );
+        }
+        result.push_back( temp );
+
+        return result;
+    }
+
+    std::vector<std::string> StringToolbox::Split( std::string const & string, std::string const & delimiter )
+    {
+        std::vector<std::string> result;
+        auto temp( string );
+        size_t pos = 0;
+
+        while ((pos = temp.find( delimiter )) != std::string::npos)
+        {
+            result.push_back( temp.substr( 0, pos ) );
+            temp.erase( 0, pos + delimiter.length() );
+        }
+        result.push_back( temp );
+
+        return result;
+    }
+
+    std::vector<std::wstring> StringToolbox::Split( std::wstring const & string, wchar_t const & delimiter )
+    {
+        std::vector<std::wstring> result;
+        auto temp( string );
+        size_t pos = 0;
+
+        while ((pos = temp.find( delimiter )) != std::wstring::npos)
+        {
+            result.push_back( temp.substr( 0, pos ) );
+            temp.erase( 0, pos + 1 );
+        }
+        result.push_back( temp );
+
+        return result;
+    }
+
+    std::vector<std::string> StringToolbox::Split( std::string const & string, char const & delimiter )
+    {
+        std::vector<std::string> result;
+        auto temp( string );
+        size_t pos = 0;
+
+        while ((pos = temp.find( delimiter )) != std::string::npos)
+        {
+            result.push_back( temp.substr( 0, pos ) );
+            temp.erase( 0, pos + 1 );
+        }
+        result.push_back( temp );
+
+        return result;
+    }
 }
--- GameTemplate/StringToolbox.h
+++ GameTemplate/StringToolbox.h
@@ -1,11 +1,54 @@
 #pragma once
-class StringToolbox
+
+#include <memory>
+#include <vector>
+
+namespace Toolbox
 {
-public:
-    StringToolbox() = delete;
-    ~StringToolbox() = delete;
+    class StringToolbox
+    {
+    public:
+        /*
+         * DO NOT USE constructors or destructors.
+         * This class should never be built and you should only
+         * use the static methods.
+         */
+        StringToolbox() = delete;
+        ~StringToolbox() = delete;
 
-    static std::wstring StringToWide(std::string const& string);
-    static std::string WideToString(std::wstring const& string);
-};
+        static std::wstring StringToWide( std::string const& string );
 
+        static std::string WideToString( std::wstring const& string );
+
+        static std::vector<std::wstring> StringToolbox::Split( std::wstring const& string, std::wstring const & delimiter );
+        static std::vector<std::string> StringToolbox::Split( std::string const & string, std::string const & delimiter );
+        static std::vector<std::wstring> StringToolbox::Split( std::wstring const& string, wchar_t const & delimiter );
+        static std::vector<std::string> StringToolbox::Split( std::string const& string, char const & delimiter );
+
+        template<typename ... Args>
+        static std::wstring Format( const std::wstring& format, Args ... args )
+        {
+            if (sizeof...(Args) > 0)
+            {
+                size_t size = std::swprintf( nullptr, 0, format.c_str(), args ... ) + 1;
+                std::unique_ptr<wchar_t[]> buf( new wchar_t[size] );
+                std::swprintf( buf.get(), size, format.c_str(), args ... );
+                return std::wstring( buf.get(), buf.get() + size - 1 );
+            }
+            return format;
+        }
+
+        template<typename ... Args>
+        static std::string Format( const std::string& format, Args ... args )
+        {
+            if (sizeof...(Args) > 0)
+            {
+                size_t size = sprintf_s( nullptr, 0, format.c_str(), args ... ) + 1;
+                std::unique_ptr<char[]> buf( new char[size] );
+                sprintf_s( buf.get(), size, format.c_str(), args ... );
+                return std::string( buf.get(), buf.get() + size - 1 );
+            }
+            return format;
+        }
+    };
+}
